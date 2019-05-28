629bd5fa96e7ee644867b04b05249962342e5fa9
Ravi Goka <ravikumargoka@gmail.com>
2019-05-28 16:49:31 -0700

    Adding the upper and lower bounds for integer.

--- ReverseNumber.java
+++ ReverseNumber.java
@@ -1,15 +1,24 @@
 public class ReverseNumber{
 
-	public static void main(String[] args){
-
-		int num = 12653;
-		int reverseNumber = 0;
-
-		while(num != 0){
-			reverseNumber  = reverseNumber*10 + num%10;
-			num /= 10;
-		}
-		System.out.println("The reverse number: "+reverseNumber);
-	}
+	public int reverse(int x) {
+        
+        int reversedNumber = 0;
+        int upperlimit = 214748364;
+        int lowerlimit = -214748364;
+        
+        while(x != 0){
+            if(reversedNumber > upperlimit){
+              return 0;
+            }
+            if(reversedNumber < lowerlimit){
+              return 0;
+            }
+            reversedNumber = reversedNumber*10 + x%10;
+            
+            x /= 10;
+        }
+        return reversedNumber; 
+        
+    }
 	
 }
