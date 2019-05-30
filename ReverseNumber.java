845952057085242bd9db2794c73aee3135075f91
Ravi Goka <ravikumargoka@gmail.com>
2019-05-30 16:29:40 -0700

    Doing in more efficient way

--- ReverseNumber.java
+++ ReverseNumber.java
@@ -1,24 +1,18 @@
 public class ReverseNumber{
 
 	public int reverse(int x) {
-        
-        int reversedNumber = 0;
-        int upperlimit = 214748364;
-        int lowerlimit = -214748364;
-        
+        double reversedNumber = 0;
         while(x != 0){
-            if(reversedNumber > upperlimit){
-              return 0;
-            }
-            if(reversedNumber < lowerlimit){
-              return 0;
-            }
             reversedNumber = reversedNumber*10 + x%10;
-            
             x /= 10;
         }
-        return reversedNumber; 
-        
+        if(reversedNumber > Integer.MAX_VALUE){
+            return 0;
+        }
+        if(reversedNumber < Integer.MIN_VALUE){
+            return 0;
+        }
+        return (int)reversedNumber;
     }
 	
 }
